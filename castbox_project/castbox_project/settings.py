417b9ca1fe8aea52f17090132c19bd846f50e8d5
ShammasAmir <amirshammas@gmail.com>
2024-04-22 00:38:34 +0330

    Create CustomUser model.Run migrations.

--- castbox_project/castbox_project/settings.py
+++ castbox_project/castbox_project/settings.py
@@ -21,6 +21,9 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+
+    # MyApps
+    "castbox.apps.CastboxConfig",
 ]
 
 MIDDLEWARE = [
@@ -105,3 +108,5 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+AUTH_USER_MODEL = "castbox.CustomUser"
