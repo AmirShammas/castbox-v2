f34f74c0e1e0cdbe73e20e45ad97d9b045f9b4ab
ShammasAmir <amirshammas@gmail.com>
2024-04-22 11:25:50 +0330

    Create Channel model.Run migrations.

--- castbox_project/castbox/admin.py
+++ castbox_project/castbox/admin.py
@@ -1,14 +1,19 @@
 from django.contrib import admin
 from django.contrib.auth import get_user_model
 from django.contrib.auth.admin import UserAdmin
+from django.contrib.admin import register
+from .models import Channel
 
 
 CustomUser = get_user_model()
 class CustomUserAdmin(UserAdmin):
     model = CustomUser
-    list_display = [
-    "email",
-    "username",
-    "is_superuser",
-    ]
+    list_display = ("id", "username", "email", "is_superuser",)
 admin.site.register(CustomUser, CustomUserAdmin)
+
+
+@register(Channel)
+class ChannelAdmin(admin.ModelAdmin):
+    list_display = ("id", "title", "is_active",)
+    list_editable = ("is_active",)
+
--- castbox_project/castbox/models.py
+++ castbox_project/castbox/models.py
@@ -1,6 +1,51 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from abc import abstractmethod
+from django.conf import settings
 
 
 class CustomUser(AbstractUser):
-    pass
+    class Meta:
+        verbose_name = "CustomUser"
+        verbose_name_plural = "CustomUsers"
+        ordering = ("id",)
+
+
+class MyBaseModel(models.Model):
+    is_active = models.BooleanField(
+        default=False,
+        verbose_name="Is Active",
+    )
+
+    created_at = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name="Created At",
+    )
+
+    updated_at = models.DateTimeField(
+        auto_now=True,
+        verbose_name="Updated At",
+    )
+
+    class Meta:
+        abstract = True
+        ordering = ("pk",)
+
+    @abstractmethod
+    def __str__(self):
+        raise NotImplementedError("Implement __str__ method")
+
+
+class Channel(MyBaseModel):
+    title = models.CharField(max_length=50, null=True, blank=True, verbose_name="Title")
+    description = models.TextField(null=True, blank=True, verbose_name="Description")
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL, null=True, blank=True, on_delete=models.CASCADE, related_name="channels", verbose_name="Owner")
+
+    class Meta:
+        verbose_name = "Channel"
+        verbose_name_plural = "Channels"
+        ordering = ("id",)
+
+    def __str__(self):
+        return self.title
+
