0797143d6a165ddfad152b62004f842c7d119a88
ShammasAmir <amirshammas@gmail.com>
2024-04-22 14:18:12 +0330

    Create channel_list page.

--- castbox_project/castbox/urls.py
+++ castbox_project/castbox/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import SignupPageView, HomePageView
+from .views import ChannelListView, SignupPageView, HomePageView
 
 urlpatterns = [
     path("accounts/signup/", SignupPageView.as_view(), name="signup"),
     path("", HomePageView.as_view(), name="home"),
+    path("channel/", ChannelListView.as_view(), name="channel_list"),
 ]
--- castbox_project/castbox/views.py
+++ castbox_project/castbox/views.py
@@ -1,7 +1,9 @@
 from django.urls import reverse_lazy
 from django.views import generic
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, ListView, DetailView
+from .models import Channel
 from .forms import CustomUserCreationForm
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 
 class SignupPageView(generic.CreateView):
@@ -12,3 +14,14 @@ class SignupPageView(generic.CreateView):
 
 class HomePageView(TemplateView):
     template_name = "home.html"
+
+
+class ChannelListView(LoginRequiredMixin, ListView):
+    model = Channel
+    context_object_name = "channel_list"
+    template_name = "channels/channel_list.html"
+    login_url = "login"
+
+    def get_queryset(self):
+        return Channel.objects.all()
+
