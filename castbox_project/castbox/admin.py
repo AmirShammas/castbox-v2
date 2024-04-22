6fad7347458f19523999e4b06d8a33c4b9568a0d
ShammasAmir <amirshammas@gmail.com>
2024-04-22 12:29:28 +0330

    Create model for Episode and Comment.Run migrations.

--- castbox_project/castbox/admin.py
+++ castbox_project/castbox/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.contrib.auth import get_user_model
 from django.contrib.auth.admin import UserAdmin
 from django.contrib.admin import register
-from .models import Channel
+from .models import Channel, Comment, Episode
 
 
 CustomUser = get_user_model()
@@ -14,6 +14,19 @@ admin.site.register(CustomUser, CustomUserAdmin)
 
 @register(Channel)
 class ChannelAdmin(admin.ModelAdmin):
-    list_display = ("id", "title", "is_active",)
+    list_display = ("id", "title", "owner", "is_active",)
     list_editable = ("is_active",)
+
+
+@register(Episode)
+class EpisodeAdmin(admin.ModelAdmin):
+    list_display = ("id", "title", "owner", "channel", "is_active",)
+    list_editable = ("is_active",)
+
+
+@register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ("id", "title", "author", "channel", "is_active",)
+    list_editable = ("is_active",)
+
 
--- castbox_project/castbox/models.py
+++ castbox_project/castbox/models.py
@@ -49,3 +49,34 @@ class Channel(MyBaseModel):
     def __str__(self):
         return self.title
 
+
+class Episode(MyBaseModel):
+    title = models.CharField(max_length=50, null=True, blank=True, verbose_name="Title")
+    description = models.TextField(null=True, blank=True, verbose_name="Description")
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL, null=True, blank=True, on_delete=models.CASCADE, related_name="episodes", verbose_name="Owner")
+    channel = models.ForeignKey(Channel, null=True, blank=True, on_delete=models.CASCADE, related_name="episodes", verbose_name="Channel")
+
+    class Meta:
+        verbose_name = "Episode"
+        verbose_name_plural = "Episodes"
+        ordering = ("id",)
+
+    def __str__(self):
+        return self.title
+
+
+class Comment(MyBaseModel):
+    title = models.CharField(max_length=50, null=True, blank=True, verbose_name="Title")
+    description = models.TextField(null=True, blank=True, verbose_name="Description")
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, null=True, blank=True, on_delete=models.CASCADE, related_name="comments", verbose_name="Author")
+    channel = models.ForeignKey(Channel, null=True, blank=True, on_delete=models.CASCADE, related_name="comments", verbose_name="Channel")
+
+    class Meta:
+        verbose_name = "Comment"
+        verbose_name_plural = "Comments"
+        ordering = ("id",)
+
+    def __str__(self):
+        return self.title
+
+
